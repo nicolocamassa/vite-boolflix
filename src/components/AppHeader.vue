f585c674985f309f034f32b481a06d5f3384996e
nicolocamassa <camassanicolo03@gmail.com>
2024-01-12 15:55:14 +0100

    [UPDATE] initial files set

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data(){
+        return{
+            
+        }
+    }
+};
+</script>
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+<style lang="scss" scoped></style>
