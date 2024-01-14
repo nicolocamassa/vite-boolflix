f88badad5f8527953a57e3c3eceb77187633af6b
nicolocamassa <camassanicolo03@gmail.com>
2024-01-14 16:13:01 +0100

    [ADD] installed flag package API

--- src/components/CardGen.vue
+++ src/components/CardGen.vue
@@ -6,27 +6,33 @@ import { store } from "../store";
 export default {
   name: "AppMain",
   props: {
-    film: Object
+    film: Object,
   },
   data() {
     return {
       store,
     };
   },
+  methods: {
+    generateFlagImageUrl(language) {
+        return `https://flagcdn.com/16x12/${language}.png`
+    }
+}
 };
 </script>
 <template lang="">
-    <div class='card'>
-        <h1>{{ film.title }}</h1>
-  <h2>{{ film.original_title }}</h2>
-  <div>{{ film.original_language }}</div>
-  <div>{{ film.vote_average }}</div> <!-- TODO: 2 DECIMALI -->
-    </div>
-  
+  <div class="card">
+    <h1>{{ film.title }}</h1>
+    <h2>{{ film.original_title }}</h2>
+    <div>{{ film.original_language }}</div>
+    <div>{{ film.vote_average }}</div> <!-- TODO: 2 DECIMALI -->
+    <img :src="generateFlagImageUrl(film.original_language)" alt="">
+    <!-- TODO: CERCARE API BANDIERE (LINGUA -> STATO -> BANDIERA [big data cloud]) o (LINGUA -> BANDIERA [non trovato])-->
+  </div>
 </template>
 <style lang="scss" scoped>
-    .card{
-        color: white;
-        margin-bottom: 40px;
-    }
+.card {
+  color: white;
+  margin-bottom: 40px;
+}
 </style>
